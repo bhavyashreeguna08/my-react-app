472e962c189d9df2aef08ff36cd99a2e13d5b016
bhavyashreeguna08 <bhavyashreeguna08@gmail.com>
2024-11-18 12:20:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function App() {
+  return (<div style={{ textAlign:"center", marginTop: "50px", backgroundColor: "pink"}}>
+    <h1>Welcome to kubernetes Application</h1>
+    <p>Iam running in k8 😀</p>
+  </div>);
+}
+
+export default App;
